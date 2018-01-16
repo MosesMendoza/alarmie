951616d5293d49923241d8c9fb1a6535ad9f8871
Moses Mendoza <moses@puppet.com>
2018-01-15 21:07:27 -0800

    Fix object references in Config
    * Config.LoadFromEnvironment and Config.LoadFromFile erroneously acted on a
      copy, not a reference, to the caller Config object. This meant that no
      changes from this these functions persisted outside the function. This commit
      changes them to references.
    
    * Also generally prefer the := syntax to var <id> = <val>, as this appears more
      concise/common.
    
    * Rename Log to logger in main - it's a more common name than Log and doesn't
      signify something its not via the capital letter
    
    Signed-off-by: Moses Mendoza <moses@puppet.com>

--- app/config.go
+++ app/config.go
@@ -14,10 +14,10 @@ type Config struct {
 
 // LoadFromEnvironment will populate the values of the given Config pointer from
 // the pre-known keys in the environment, and return non-nil error on failure
-func (c Config) LoadFromEnvironment() error {
+func (config *Config) LoadFromEnvironment() error {
 	const logFilePath = "ALARMIE_LOGFILEPATH"
-	c.LogFilePath = os.Getenv("ALARMIE_LOGFILEPATH")
-	if c.LogFilePath == "" {
+	config.LogFilePath = os.Getenv("ALARMIE_LOGFILEPATH")
+	if config.LogFilePath == "" {
 		return fmt.Errorf("Could not locate values for configuration in environment: %s", logFilePath)
 	}
 	return nil
@@ -26,14 +26,14 @@ func (c Config) LoadFromEnvironment() error {
 // LoadFromFile will populate the values of the given Config pointer from the
 // JSON file located at the supplied path. Will return a non-nil error on
 // failure.
-func (c Config) LoadFromFile(configFilePath string) error {
+func (config *Config) LoadFromFile(configFilePath string) error {
 	var handle, error = ioutil.ReadFile(configFilePath)
 	if error != nil {
 		fmt.Printf("Could not load configuration from file %s: %s", configFilePath, error.Error())
 		return error
 	}
 
-	var unmarshalError = json.Unmarshal(handle, &c)
+	var unmarshalError = json.Unmarshal(handle, config)
 	if unmarshalError != nil {
 		fmt.Printf("Could not deserialize JSON config file %s: %s", configFilePath, error.Error())
 	}
--- app/main.go
+++ app/main.go
@@ -12,7 +12,7 @@ func main() {
 	var application = new(AlarmieApplication)
 
 	// set up configuration
-	var config = new(Config)
+	config := new(Config)
 	var configError = config.LoadFromEnvironment()
 	if configError != nil {
 		panic(errors.New("Could not locate values for configuration in environment"))
@@ -21,17 +21,15 @@ func main() {
 	application.Config = config
 
 	// set up logger
-	var Log = log.New("Alarmie", "application startup")
+	logger := log.New("Alarmie", "application startup")
 	// TODO: Have to actually make the log file on first run
-	var logHandler, error = log.FileHandler(config.LogFilePath, log.LogfmtFormat())
-	fmt.Println(config)
+	logHandler, error := log.FileHandler(config.LogFilePath, log.LogfmtFormat())
 	if error != nil {
 		panic(fmt.Errorf("Could not open log file handle: %s", error.Error()))
 	} else {
-		Log.SetHandler(logHandler)
-		application.Logger = Log
+		logger.SetHandler(logHandler)
+		application.Logger = logger
 	}
-	// this doesn't work right
 	application.Logger.Warn("Started application")
 	// initiate connection to slack
 }
