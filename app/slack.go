2682240ad0c0fbbca1678e5db011ea36fc0f0f17
Moses Mendoza <moses@puppet.com>
2018-01-20 17:03:10 -0800

    Add Slack API models
    Add types for Message and Rtm connection (real-time messaging) and member
    fields. We're going to start with RTM and see if it meets the need. If not, we
    can expand to the event-based API.
    
    Signed-off-by: Moses Mendoza <moses@puppet.com>

--- app/slack.go
+++ app/slack.go
@@ -29,3 +29,35 @@ func (s SlackConnection) SendMessage(message string) bool {
 func (s SlackConnection) GetMessage() string {
 	return "foo"
 }
+
+// Models, particularly Slack API
+
+// Message corresponds 1:1 to the slack Message object
+type Message struct {
+	Type      string `json:"type"`    // I think this is always "message"?
+	Channel   string `json:"channel"` // name of slack channel message was on
+	User      string `json:"user"`    // user sending message
+	Text      string `json:"text"`    // content of message
+	Timestamp string `json:"ts"`      // timestamp
+}
+
+// RtmConnectResponse corresponds to the API response from rtm.connect
+type RtmConnectResponse struct {
+	Ok    bool           `json:"ok"`   // did we successfully connect
+	Self  RtmConnectSelf `json:"self"` // contains id, name response
+	Team  RtmConnectTeam `json:"team"` // info on team
+	WsURL string         `json:"url"`  // websocket url to connect to - in <= 30 seconds
+}
+
+// RtmConnectSelf is one of the member fields on RtmConnectResponse
+type RtmConnectSelf struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+// RtmConnectTeam is one of the member fields on RtmConnectReponse
+type RtmConnectTeam struct {
+	Domain string `json:"domain"`
+	ID     string `json:"id"`
+	Name   string `json:"name"`
+}
