11d11331d3897ef7c3791f58ce5acb9f27c5bcbe
Moses Mendoza <moses@puppet.com>
2018-01-24 07:05:05 -0800

    Start wiring up testUtils http server in slack_test
    Use slack_test to develop the http server in our testUtils package. Right now
    we're just testing that we can achieve bi-directional communication over
    websocket. Next we'll make it so that our test http server can simulate Slack
    by returning the message we expect Slack to on dial.
    
    Signed-off-by: Moses Mendoza <moses@puppet.com>

---
+++ app/slack_test.go
@@ -0,0 +1,42 @@
+package main
+
+import "testing"
+import "github.com/MosesMendoza/alarmie/testUtils"
+import "golang.org/x/net/websocket"
+import "fmt"
+
+func TestInitiatWebSocketConnectionCreatesWebsocket(t *testing.T) {
+	/*
+		Test that, given a URL, uses websocket to "dial" that URL and initiate
+		a socket connection. then returns the socket connection
+
+		Test by creating an http server that serves websocket connections, and testing
+		that the connection is returned
+	*/
+
+	server := testUtils.StartTestWebsocketServer()
+
+	defer testUtils.StopTestWebsocketServer(server)
+
+	fmt.Printf("Dialing " + "ws://127.0.0.1" + server.Addr + "\n")
+	connection, error := websocket.Dial("ws://127.0.0.1"+server.Addr, "", "http://localhost")
+
+	if error != nil {
+		fmt.Printf("Could not dial websocket connection in slack_test: %s", error.Error())
+		t.FailNow()
+	}
+	message := "Test Message"
+	var reply string
+
+	sendError := websocket.Message.Send(connection, message)
+	if sendError != nil {
+		fmt.Printf("Could not send test message over websocket in slack_test: %s", sendError.Error())
+		t.FailNow()
+	}
+
+	receiveError := websocket.Message.Receive(connection, &reply)
+	if receiveError != nil {
+		fmt.Printf("Could not receive reply message over websocket in slack_test: %s", receiveError.Error())
+		t.FailNow()
+	}
+}
